5d67988370128626653fadfe91651e9f0856e48b
wodrow <wodrow451611cv@gmail.com>
2017-07-03 19:26:40 +0800

    init

--- frontend/controllers/SiteController.php
+++ frontend/controllers/SiteController.php
@@ -63,7 +63,9 @@ class SiteController extends Controller
             ],
             'captcha' => [
                 'class' => 'yii\captcha\CaptchaAction',
-                'fixedVerifyCode' => YII_ENV_TEST ? 'testme' : null,
+                'minLength' => 4,
+                'maxLength' => 4,
+                'fontFile' => '@data/fonts/ztgjkt.ttf',
             ],
         ];
     }
--- frontend/views/layouts/main.php
+++ frontend/views/layouts/main.php
@@ -39,8 +39,8 @@ AppAsset::register($this);
 //        ['label' => 'Home', 'url' => ['/site/index']],
     ];
     if (Yii::$app->user->isGuest) {
-        $menuItems[] = ['label' => 'Signup', 'url' => ['/site/signup']];
-        $menuItems[] = ['label' => 'Login', 'url' => ['/site/login']];
+        $menuItems[] = ['label' => '注册', 'url' => ['/site/signup']];
+        $menuItems[] = ['label' => '登录', 'url' => ['/site/login']];
     } else {
         $menuItems[] = '<li>'
             . Html::beginForm(['/site/logout'], 'post')
