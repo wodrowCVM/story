137775266059c68054d0db137345f3ff1e0227bf
wodrow <wodrow451611cv@gmail.com>
2017-07-04 19:39:32 +0800

    update

--- frontend/config/main.php
+++ frontend/config/main.php
@@ -9,7 +9,7 @@ $params = array_merge(
 return [
     'id' => 'frontend',
     'basePath' => dirname(__DIR__),
-    'name' => 'YY故事',
+    'name' => '故事',
     'bootstrap' => ['log'],
     'controllerNamespace' => 'frontend\controllers',
     'components' => [
